c82c29ef18a07ff9a6105f9e14d0d216c5d08c1f
Mehul Kothari <mehulkothari05@gmail.com>
2019-04-11 10:10:26 +0530

    Chap App

---
+++ ChatApp/app/src/main/java/com/chatapp/UserDetails.java
@@ -0,0 +1,12 @@
+package com.chatapp;
+
+/**
+ * Created by deepa on 12-05-2018.
+ */
+
+public class UserDetails {
+
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
